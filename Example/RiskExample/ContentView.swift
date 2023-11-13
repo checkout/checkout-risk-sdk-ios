1aa0e60503ba8b350c0c521bc475d3d0be49cd6b
Precious OSSAI <precious.ossai@checkout.com>
2023-11-13 19:29:29 +0000

    feat: improve iOS example UI

--- Example/RiskExample/ContentView.swift
+++ Example/RiskExample/ContentView.swift
@@ -11,29 +11,34 @@ import Risk
 import Foundation
 
 struct ContentView: View {
-    
-    var body: some View {
-        
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Risk iOS")
-            
-            Button("Initialise Risk") {
-                let yourConfig = RiskConfig(publicKey: "pk_qa_7wzteoyh4nctbkbvghw7eoimiyo", environment: RiskEnvironment.qa)
-                
-                Risk.createInstance(config: yourConfig) { 
-                    riskInstance in
-                    riskInstance?.publishData() {
-						response in
-						deviceSessionId = response?.deviceSessionId ?? ""
-					}
-                }
-        }
-        .padding()
-    }
+	@State private var deviceSessionId: String = ""
+	@State private var checked: Bool = false
+	
+	var body: some View {
+		Text("Risk iOS Example").padding(.bottom).frame(maxWidth: .infinity, alignment: .center).font(.title)
+		
+		VStack(alignment: .leading) {
+			
+			Text("Card no: 0000 1234 6549 15151")
+			Text("Card exp: 12/26")
+			Text("Card CVV: 500").padding(.bottom)
+			
+		}
+		.padding().background(Color.gray.opacity(0.1))
+		
+		Button("Pay $1400") {
+			let yourConfig = RiskConfig(publicKey: "pk_qa_7wzteoyh4nctbkbvghw7eoimiyo", environment: RiskEnvironment.qa)
+			
+			Risk.createInstance(config: yourConfig) { riskInstance in
+				riskInstance?.publishData() { response in
+					checked = true
+					deviceSessionId = response?.deviceSessionId ?? ""
+				}
+			}
+		}.padding().background(Color.blue.opacity(0.9)).cornerRadius(8).frame(maxWidth: .infinity, alignment: .center).foregroundColor(.white).padding(.top)
+		
+		Text(!checked ? "" : "Device session id: \(deviceSessionId)").padding(.top).multilineTextAlignment(.center)
+	}
 }
 
 #Preview {
