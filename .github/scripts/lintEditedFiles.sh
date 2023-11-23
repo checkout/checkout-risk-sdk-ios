a0b760b44db2ba5079ba632f7e14495add359937
Precious OSSAI <precious.ossai@checkout.com>
2023-11-23 17:05:41 +0000

    refactor

--- .github/scripts/lintEditedFiles.sh
+++ .github/scripts/lintEditedFiles.sh
@@ -7,26 +7,18 @@ echo "Fetched"
 # Run SwiftLint
 START_DATE=$(date +"%s")
 
-SWIFT_LINT=/opt/homebrew/bin/swiftlint
-
-
 # Run SwiftLint for given filename
 run_swiftlint() {
     local filename="${1}"
     if [[ "${filename##*.}" == "swift" ]]; then
-        ${SWIFT_LINT} -- "${filename}"
+        swiftlint -- "${filename}"
     fi
 }
 
-if [[ -e "${SWIFT_LINT}" ]]; then
-    echo "SwiftLint version: $(${SWIFT_LINT} version)"
-    # Run for both staged and unstaged files
-    git diff --name-only | while read filename; do run_swiftlint "${filename}"; done
-    git diff --cached --name-only | while read filename; do run_swiftlint "${filename}"; done
-else
-    echo "${SWIFT_LINT} is not installed."
-    exit 0
-fi
+# Run for both staged and unstaged files
+git diff --name-only | while read filename; do run_swiftlint "${filename}"; done
+git diff --cached --name-only | while read filename; do run_swiftlint "${filename}"; done
+
 
 END_DATE=$(date +"%s")
 
