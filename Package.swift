df39824829ddaa36e1348aca3e5b51ec9d08a01a
Precious OSSAI <precious.ossai@checkout.com>
2023-12-06 13:56:50 +0000

    feat: add swiftLanguageVersion to SPM config

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Risk",
             targets: ["Risk"])
@@ -25,8 +24,6 @@ let package = Package(
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Risk",
             dependencies: [
@@ -40,5 +37,6 @@ let package = Package(
         .testTarget(
             name: "RiskTests",
             dependencies: ["Risk"])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
