457a94eda99e105ea7092e56f9733c8882898ac2
Precious OSSAI <precious.ossai@checkout.com>
2024-07-10 11:43:36 +0100

    chore: cleanup

--- Tests/RiskTests/RiskTests.swift
+++ Tests/RiskTests/RiskTests.swift
@@ -10,43 +10,7 @@ import XCTest
 @testable import Risk
 
 class RiskTests: XCTestCase {
-    
-//    func testGetInstanceWithValidConfiguration() {
-//        let expectation = self.expectation(description: "Risk instance creation")
-//        
-//        // TODO: Update CI to pass environment variable
-//        //          guard let publicKey = ProcessInfo.processInfo.environment["SAMPLE_MERCHANT_PUBLIC_KEY"] else {
-//        //              XCTFail("Environment variable SAMPLE_MERCHANT_PUBLIC_KEY is not set.")
-//        //              return
-//        //          }
-//        let publicKey = "dummy_key"
-//        let validConfig = RiskConfig(publicKey: publicKey, environment: RiskEnvironment.qa)
-//        let riskSDK = Risk(config: validConfig)
-//        
-//        riskSDK.configure { [weak self] result in
-//            guard let _ = self else { return }
-//            
-//            switch result {
-//            case .success:
-//                riskSDK.publishData { result in
-//                    switch result {
-//                    case .success(let response):
-//                        XCTAssertNotNil(response.deviceSessionId)
-//                        expectation.fulfill()
-//                    case .failure:
-//                        XCTFail("Risk data published failed")
-//                        expectation.fulfill()
-//                    }
-//                }
-//            case .failure(let error):
-//                XCTFail("Failed to configure Risk SDK with valid configuration: \(error)")
-//            }
-//        }
-//        
-//        waitForExpectations(timeout: 5, handler: nil)
-//        XCTAssertNotNil(riskSDK)
-//    }
-    
+
     func testGetInstanceWithInvalidPublicKey() {
         let expectation = self.expectation(description: "Risk instance creation with invalid public key")
         
@@ -59,49 +23,7 @@ class RiskTests: XCTestCase {
         
         waitForExpectations(timeout: 5, handler: nil)
     }
-    
-    //    func testPublishTimeout() {
-    //        let expectation = self.expectation(description: "Risk data timed out")
-    //
-    //    let publicKey = "dummy_key"
-    //        let validConfig = RiskConfig(publicKey: publicKey, environment: RiskEnvironment.qa)
-    //
-    //
-    //
-    //        let riskSDK = Risk(config: validConfig)
-    //
-    //        riskSDK.fingerprintTimeoutInterval = 0.01
-    //
-    //        riskSDK.configure { [weak self] result in
-    //            guard let _ = self else { return }
-    //
-    //            switch result {
-    //            case .success:
-    //                let stubFingerprintService = MockFingerprintService()
-    //                stubFingerprintService.delayTime = 0.1
-    //
-    //                riskSDK.fingerprintService = stubFingerprintService
-    //
-    //                riskSDK.publishData { result in
-    //                    switch result {
-    //                    case .success:
-    //                        XCTFail("Risk data published without timeout")
-    //                        expectation.fulfill()
-    //                    case .failure(let error):
-    //                        XCTAssertEqual(error, .fingerprintTimeout)
-    //                        expectation.fulfill()
-    //                    }
-    //                }
-    //            case .failure(let error):
-    //                XCTFail("Failed to configure Risk SDK with valid configuration: \(error)")
-    //            }
-    //        }
-    //
-    //        waitForExpectations(timeout: 5, handler: nil)
-    //    }
-    
-    
-    
+
     func testPublishTimeout() {
         verifyFingerprintTimeout(shouldTimeout: true, fingerprintTimeoutInterval: 0.01, delayTime: 0.1)
     }
