271d39dcf98141c27cfb53f3359b351adba3f230
James Lappin <james.lappin@checkout.com>
2025-03-03 08:57:28 +0000

    updating fingerprint

--- Sources/Risk/Logging/Constants.swift
+++ Sources/Risk/Logging/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Constants {
     static let productName = "risk-ios-sdk"
-    static let riskSdkVersion = "3.0.3"
+    static let riskSdkVersion = "3.0.4"
     static let userAgent = "checkout-sdk-risk-ios/\(riskSdkVersion)"
     static let loggerTypeIdentifier = "com.checkout.risk-mobile-sdk"
 }
